a18ad0b2b01960f61943e5c95c84bc9c77f807ab
moomoon <gmoomoon@gmail.com>
2015-09-26 20:54:54 +0800

    Initial Commit

---
+++ MiaoPaiVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiaoPaiVideo
+//
+//  Created by Phoebe Hu on 9/26/15.
+//  Copyright (c) 2015 Phoebe Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
